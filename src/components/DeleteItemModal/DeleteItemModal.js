a37b848b2cf608d480fc703c7f7e98e05a2dab7d
XDRO <joshuaarcher76@gmail.com>
2024-02-01 16:10:20 -0600

    remove comments

--- src/components/DeleteItemModal/DeleteItemModal.js
+++ src/components/DeleteItemModal/DeleteItemModal.js
@@ -20,38 +20,3 @@ const DeleteItemModal = ({ onClose, deleteCard }) => {
 };
 
 export default DeleteItemModal;
-
-{
-  /* <div className="modal__overlay">
-       <div className={`modal delete`}>
-        <div className="delete__modal">
-          <div className="delete__modal-header">
-            <button className="delete__modal-close">
-              <img src={closeButton} onClick={onClose} alt="Close Button"></img>
-            </button>
-          </div>
-
-          <div className="delete__modal-content">
-            <p className="delete__modal-text">
-              Are you sure you want to delete this item?
-            </p>
-            <p className="delete__modal-text">This action is irreversible.</p>
-          </div>
-          <div className="delete__modal-footer">
-            <button
-              className="delete_btn delete_btn-confirm"
-              onClick={deleteCard}
-            >
-              Yes, delete item
-            </button>
-            <button className="delete_btn delete_btn-cancel" onClick={onClose}>
-              Cancel
-            </button>
-          </div>
-        </div>
-      </div> 
-</div> */
-}
-
-{
-}
