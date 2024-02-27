180e70253ba88b7bce0624d0765bb8d83b498967
XDRO <joshuaarcher76@gmail.com>
2024-02-27 13:29:32 -0600

    comment out browserRouter inside of index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import {
-  BrowserRouter,
+  // BrowserRouter,
   HashRouter,
 } from "react-router-dom/cjs/react-router-dom.min";
 
